0f66512a6deaba4723e3d09d892abba7cae129e5
Muhammad Amirul Haq <muhammadamirulhaq66@gmail.com>
2020-11-07 08:43:53 +0800

    Random requested was added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+document.querySelector("body").style.background = "url(assets/img/space2.jpg)";
+document.querySelector("header").style.background = "url(assets/img/space.jpg)";
+
 const aturan = alert("ATURAN PERMAINAN :\n\n1. Pemain harus menembak bola sesuai dengan warna yang diminta.\n2. Apabila pemain tepat sasaran, maka score akan bertambah 1.\n3. Apabila pemain tidak tepat sasaran, maka score akan berkurang 2.\n\nSelamat mencoba!");
 
 const bolaMerah = document.querySelector(".bolaMerah");
@@ -15,32 +18,34 @@ let tembakBola = document.querySelector(".tembakBola");
 
 myScore.innerHTML = "0";
 
-let tembakanBolaWarna = ["Merah", "Kuning", "Hijau"];
+let tembakanBolaWarna = ["Bumi", "Jupiter", "Mars"];
 tembakBola.innerHTML = tembakanBolaWarna[2];
 
 let newScore = myScore.innerHTML;
+
+let iRandom = 0;
 
 function scoreBolaMerah() {
 
     audio.play();
 
-    if(newScore < 10) {
+    if(newScore < 20) {
         marq.setAttribute("scrollamount", "12");
         marq2.setAttribute("scrollamount", "10");
         marq3.setAttribute("scrollamount", "14");
-    } else if(newScore >= 10 && newScore < 20) {
+    } else if(newScore >= 20 && newScore < 40) {
         marq.setAttribute("scrollamount", "18");
         marq2.setAttribute("scrollamount", "20");
         marq3.setAttribute("scrollamount", "16");
-    } else if(newScore >= 20 && newScore < 30) {
+    } else if(newScore >= 40 && newScore < 70) {
         marq.setAttribute("scrollamount", "26");
         marq2.setAttribute("scrollamount", "22");
         marq3.setAttribute("scrollamount", "24");
-    } else if(newScore >= 20 && newScore < 30) {
+    } else if(newScore >= 70 && newScore < 80) {
         marq.setAttribute("scrollamount", "30");
         marq2.setAttribute("scrollamount", "28");
         marq3.setAttribute("scrollamount", "32");
-    } else if(newScore >= 30 && newScore < 40) {
+    } else if(newScore >= 80 && newScore < 100) {
         marq.setAttribute("scrollamount", "36");
         marq2.setAttribute("scrollamount", "38");
         marq3.setAttribute("scrollamount", "34");
@@ -58,13 +63,10 @@ function scoreBolaMerah() {
 
     myScore.innerHTML = newScore;
 
-    if(tembakBola.innerHTML === tembakanBolaWarna[0]) {
-        tembakBola.innerHTML = tembakanBolaWarna[1];
-    } else if(tembakBola.innerHTML === tembakanBolaWarna[1]) {
-        tembakBola.innerHTML = tembakanBolaWarna[2];
-    } else {
-        tembakBola.innerHTML = tembakanBolaWarna[0];     
-    }
+    let random = [0, 1, 2, 0, 2, 0, 1, 2, 0, 1, 0, 2, 1, 0, 1, 0, 2, 1, 0, 1];
+
+    tembakBola.innerHTML = tembakanBolaWarna[random[iRandom % random.length]];
+    iRandom++;
 }
 
 function scoreBolaKuning() {
@@ -105,13 +107,10 @@ function scoreBolaKuning() {
 
     myScore.innerHTML = newScore;
 
-    if(tembakBola.innerHTML === tembakanBolaWarna[0]) {
-        tembakBola.innerHTML = tembakanBolaWarna[1];
-    } else if(tembakBola.innerHTML === tembakanBolaWarna[1]) {
-        tembakBola.innerHTML = tembakanBolaWarna[2];
-    } else {
-        tembakBola.innerHTML = tembakanBolaWarna[0];     
-    }
+    let random = [0, 1, 2, 0, 2, 0, 1, 2, 0, 1, 0, 2, 1, 0, 1, 0, 2, 1, 0, 1];
+
+    tembakBola.innerHTML = tembakanBolaWarna[random[iRandom % random.length]];
+    iRandom++;
 }
 
 function scoreBolaHijau() {
@@ -152,13 +151,10 @@ function scoreBolaHijau() {
 
     myScore.innerHTML = newScore;
 
-    if(tembakBola.innerHTML === tembakanBolaWarna[0]) {
-        tembakBola.innerHTML = tembakanBolaWarna[1];
-    } else if(tembakBola.innerHTML === tembakanBolaWarna[1]) {
-        tembakBola.innerHTML = tembakanBolaWarna[2];
-    } else {
-        tembakBola.innerHTML = tembakanBolaWarna[0];     
-    }
+    let random = [0, 1, 2, 0, 2, 0, 1, 2, 0, 1, 0, 2, 1, 0, 1, 0, 2, 1, 0, 1];
+
+    tembakBola.innerHTML = tembakanBolaWarna[random[iRandom % random.length]];
+    iRandom++;
 }
 
 bolaMerah.onclick = scoreBolaMerah;
